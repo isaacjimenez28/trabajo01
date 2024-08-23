293b7b0e868242d51a3c48b397c777b4a9f5171f
isaacjimenez28 <isaacvalentinjimenezgil@gmail.com>
2024-08-22 20:12:02 -0600

    Add files via upload

---
+++ parcial 3/listrace/assets/js/script.js
@@ -0,0 +1,11 @@
+
+const file = document.getElementById(foto);
+file.addEventListener('change' e => {
+    console.log( e.target.file );
+
+const reader = new FileReader();
+reader.onload = function( e )
+
+img.src='C:\Users\LCPB15\Desktop\parcial 3\listrace\assets\images\explore';
+
+});
